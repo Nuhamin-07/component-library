f2cdd5b9ddf52740cede062a78f391fcd7869810
Dereje Tessema <dtessema@myfuse1.education>
2025-07-17 06:24:19 -0400

    resolved link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router";
 export default function Header() {
   return (
     <div className="header">
-      <a href="/">Badges</a>
-      <a href="/banners">Banners</a>
-      <a href="/Cards">Cards</a>
-      <a href="/testimonials">Testimonials</a>
+      <Link to="/">Badges</Link>
+      <Link to="/banners">Banners</Link>
+      <Link to="/Cards">Cards</Link>
+      <Link to="/testimonials">Testimonials</Link>
       {/* <a href="/tooltips">Tooltips</a>
       <a href="/toasts">Toasts</a> */}
     </div>
